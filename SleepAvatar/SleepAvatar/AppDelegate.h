d4b7e3b0232c9e18dae28974a3aa99745baafbd6
Panupat <panupatnew@gmail.com>
2015-01-15 20:35:14 +0700

    Create project

---
+++ SleepAvatar/SleepAvatar/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SleepAvatar
+//
+//  Created by panupatnew on 1/15/2558 BE.
+//  Copyright (c) 2558 medunla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SleepAvatar/SleepAvatar/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SleepAvatar
+//
+//  Created by panupatnew on 1/15/2558 BE.
+//  Copyright (c) 2558 medunla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
